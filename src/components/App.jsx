9e84cb48ca1449b0d6d01acf3f2007b7b9061472
Aleksandr Cheverda <alexar@ua.fm>
2022-11-06 19:41:45 +0200

    upd files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import Counter from "./Counter/Counter";
+
+
+
+export const App = () => (
+  <>
+    <h1>Стан компоненту</h1>
+    <Counter />
+  </>
+);
+
+export default App;

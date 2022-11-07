0e012c993158af93d6da05e7c2c6ce8f407f06ad
Aleksandr Cheverda <alexar@ua.fm>
2022-11-07 22:10:42 +0200

    upd components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { Component } from "react";
+import { components } from "react";
 import Counter from "./Counter/Counter";
 
+const App = () => (
+  <>
+    <h1>Стан компонента</h1>
+    <Counter initialValue={10} />
+  </>
+);
 
-
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0
+//   };
 
   
-}
+// }
 export default App;

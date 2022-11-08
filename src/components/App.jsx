3c9154d4edeec7a6d4b2dec178d7c6e465b597a6
Aleksandr Cheverda <alexar@ua.fm>
2022-11-08 21:15:33 +0200

    feedback final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,67 @@
-import React from "react";
-// import { components } from "react";
-import Counter from "./Counter/Counter";
+import React, { Component } from "react";
+import Section from "./Section/Section";
+import Notification from "./Notification/Notification";
+import Statistics from "./Statistics/Statistics";
+import Feedback from "./Feedback/Feedback";
+import style from './App.module.css'
 
-const App = () => (
-  <>
-    <h1>Стан компонента</h1>
-    <Counter initialValue={10} />
-  </>
-);
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0
-//   };
+  handleIncrement = option => {
+    this.setState(prevState => {
+      return { [option]: prevState[option] + 1 };
+    });
+  };
 
-  
-// }
-export default App;
+  countTotalFeedback() {
+    const totalValue = Object.values(this.state);
+    return totalValue.reduce((acc, value) => acc + value);
+  }
+
+  countPositiveFeedbackPercentage() {
+    const value = this.countTotalFeedback();
+    const percentage = Math.round((this.state.good / value) * 100);
+
+    if (!percentage) return 0;
+    return percentage;
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const stateName = Object.keys(this.state);
+    const value = this.countTotalFeedback();
+    const percent = this.countPositiveFeedbackPercentage();
+
+    console.log(value);
+
+    return (
+      <div className={style.wrapper}>
+        <Section title="Please leave feedback">
+          <Feedback
+            options={stateName}
+            onLeaveFeedback={this.handleIncrement}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {value === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={value}
+              positivePercentage={percent}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
